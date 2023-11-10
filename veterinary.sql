6a48728219548d7e4ad3cd4668cf2a44359d31cd
jhunmicko <micko.sanidad25@gmail.com>
2023-11-10 20:58:26 +0800

    feat/create-table-animals

--- veterinary.sql
+++ veterinary.sql
@@ -1,1 +1,15 @@
 CREATE DATABASE veterinary;
+-- tables
+
+--feat/create-table-animals
+CREATE TABLE animals (
+    animalid INT,
+    name VARCHAR(255) NOT NULL,
+    species VARCHAR(255) NOT NULL,
+    breed VARCHAR(255),
+    dateofbirth DATE,
+    gender VARCHAR(255),
+    color VARCHAR(255),
+    ownerid INT,
+    PRIMARY KEY (animalid)
+);
